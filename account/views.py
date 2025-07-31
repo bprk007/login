7c38441ea0499b4f59bad33b998ec30813141d5a
bprk007 <2022kucp1011@iiitkota.ac.in>
2025-07-31 18:33:58 +0530

    password rules

--- account/views.py
+++ account/views.py
@@ -1,38 +1,45 @@
+import re
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.template import loader
 from django.http import HttpResponse
-from django.contrib.auth import login,authenticate,logout
-from django.shortcuts import render,redirect
-# Create your views here.
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import redirect
 
-
+def is_valid_password(password):
+    # At least 8 characters, 1 uppercase, 1 lowercase, 1 digit, 1 special character
+    return (
+        len(password) >= 8 and
+        re.search(r"[A-Z]", password) and
+        re.search(r"[a-z]", password) and
+        re.search(r"[0-9]", password) and
+        re.search(r"[^A-Za-z0-9]", password)
+    )
 
 def register_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        user = User.objects.filter(username = username)
-
-        if user.exists():
-            messages.info(request,'User with this username already exists')
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'User with this username already exists')
             return redirect("/auth/register/")
         
-        user = User.objects.create_user(username = username)
-        
-        user.set_password(password)
+        if not is_valid_password(password):
+            messages.info(request, 'Password must be at least 8 characters long and include uppercase, lowercase, digit, and special character.')
+            return redirect("/auth/register/")
 
+        user = User.objects.create_user(username=username)
+        user.set_password(password)
         user.save()
 
-        messages.info(request,'User successfully registered, Proceed to login')
+        messages.info(request, 'User successfully registered, Proceed to login')
         return redirect("/auth/login/")
-    
 
     template = loader.get_template('register.html')
     context = {}
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
 
 def login_user(request):
 
